54835a545e7f5934a99332422b4947b6e946ef28
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-09-25 20:47:09 +0200

    page: add fade animations Make code more flexible

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 var introduction = document.getElementById("introduction");
-var summary = document.getElementById("summary");
+var sommaire = document.getElementById("sommaire");
 var informations = document.getElementById("informations");
 var viewer = document.getElementById("viewer");
-viewer.src = "blank.html";
+viewer.src = "./blank.html";
+var btn_page = {
+    "introduction": "./introduction.html",
+    "sommaire": "./sommaire.html",
+    "informations": "./informations.html"
+}
 
 function toggleButton(el) {
+    viewer.classList.toggle("hide");
     introduction.classList = ["button"];
-    summary.classList = ["button"];
+    sommaire.classList = ["button"];
     informations.classList = ["button"];
     el.classList = ["button-active"];
-    viewer.src = "introduction.html";
+    setTimeout(function () {
+        viewer.src = btn_page[el.id];
+    }, 250)
+
+    setTimeout(function () {
+        viewer.classList.toggle("hide");
+    }, 500)
 }
