488c967393af98110857e194c571fc44cdb25e0a
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-10-05 18:40:59 +0200

    Change target to target_page to comply with W3C

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ window.onload = function () { loadButtons(document); } // Initialise les boutons
 function loadButtons(el) { // fonction d'initalisation des boutons
     var elements = el.getElementsByTagName('*'), i; // récupère tous les éléments de la page et stock dans une variable
     for (i in elements) { // En math, ça donne i --> n elements --> U elements[i] = U(n) = un élément de la page en particulier
-        if (elements[i].hasAttribute && elements[i].hasAttribute('target')) { //Si l'element à des attributs et si il a un attribut target
+        if (elements[i].hasAttribute && elements[i].hasAttribute('target_page')) { //Si l'element à des attributs et si il a un attribut target_page
             elements[i].onclick = function () {
                 toggleButton(this); // execute la fonction qui charge les pages et maintient les boutons
             }
@@ -20,18 +20,18 @@ function loadButtons(el) { // fonction d'initalisation des boutons
 function toggleButton(el) { // définit fonction qui charge les pages et maintient les boutons activés (en bleu)
     var elements = document.getElementsByTagName('*'), i; // récupère tous les éléments sous el
     for (i in elements) {
-        if (elements[i].hasAttribute && elements[i].hasAttribute('target')) {
+        if (elements[i].hasAttribute && elements[i].hasAttribute('target_page')) {
             elements[i].classList = ["button"]; // On supprime toutes les classes CSS appliquées au boutton et on met "buttton" à la place
         }
     }
     pageContent.classList.toggle("hide"); // cache toi
     el.classList = ["button-active"]; // Dit au bouton qui vient d'etre cliqué de s'activer (il se met en bleu et décalé)
     setTimeout(function () { // Attend 200 ms avant d'executer la fonction (le temps que pageContent disparaisse)
-        setContentText(`${host}/web_history/${el.getAttribute("target")}`); // Probablement la ligne la plus complexe, mais facile si on la décompose:
-        //  Partie 1 : `${host}/web_history/${el.getAttribute("target")}` ce code construit l'url à charger :
+        setContentText(`${host}/web_history/${el.getAttribute("target_page")}`); // Probablement la ligne la plus complexe, mais facile si on la décompose:
+        //  Partie 1 : `${host}/web_history/${el.getAttribute("target_page")}` ce code construit l'url à charger :
         //      host : https://claj-ndc.github.io
         //      /web_history/
-        //      el.getAttribute("target") : le contenu de target du bouton sur lequel on vient de cliquer (ex : pages/informations.html)
+        //      el.getAttribute("target_page") : le contenu de target_page du bouton sur lequel on vient de cliquer (ex : pages/informations.html)
         //    Resultat : https://claj-ndc.github.io/web_history/pages/informations.html
         //  Partie 2:
         //      setContentText(url) : fonction créée par mes soins qui copie le contenu de la page située à "url" dans le div pageContent
